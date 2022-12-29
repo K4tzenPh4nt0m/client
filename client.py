962a9da634d2c69bac089c362f4e02a3a9fcd475
K4tzenPh4nt0m <105169832+K4tzenPh4nt0m@users.noreply.github.com>
2022-12-29 22:03:12 +0100

    Add files via upload

---
+++ client.py
@@ -0,0 +1,12 @@
+import socket
+
+
+SERVER_IP = '192.168.2.213'
+SERVER_PORT = 5678
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((SERVER_IP, SERVER_PORT))
+    data = s.recv(1024)
+    print(data)
+
+input()
